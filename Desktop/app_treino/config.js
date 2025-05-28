3f5e7b4b0d11f2c24ce16b379d2f89b0bcf33de6
pedrohmarconato <pedrohmarconato@gmail.com>
2025-05-28 14:02:37 -0300

    v4.-

--- Desktop/app_treino/config.js
+++
@@ -1,5 +0,0 @@
-// NÃO versionar este arquivo!
-window.SUPABASE_CONFIG = {
-  url: 'https://ktfmktecvllyiqfkavdn.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0Zm1rdGVjdmxseWlxZmthdmRuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgwNDQ2OTAsImV4cCI6MjA2MzYyMDY5MH0.PgEnBgtdITsb_G_ycSOaXOmkwNfpvIWVOOAsVrm2zCY'
-};
